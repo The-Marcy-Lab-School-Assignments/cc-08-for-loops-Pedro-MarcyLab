d5ac9275c73a954a812f739c9c9d18418834f499
Pedro Delgado <pedro.delgado2017@mesacharter.org>
2021-10-26 14:28:01 +0000

    code challenge complete

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 //1
 function countToTwenty(){
+  for(let i = 1; i <= 20; i++){
+      console.log(i)
+  }
   
 }
 
 //2
 function countFromTwenty(){
-  
+  for(let i = 20; i > 0; i--){
+      console.log(i)
+  }
 }
 
 //3
-function countToN(){
+function countToN(n){
+    for(let i = 1; i <= n; i++){
+        console.log(i)
+    }
   
 }
 
 
 //4
-function countFromN(){
-  
+function countFromN(n){
+  for(let i = n; i > 0; i--){
+      console.log(i)
+  }
 }
 
 //5
-function countEveryOdd(){
-  
+function countEveryOdd(int){
+  for(let i = 1; i <= int; i++){
+      if(i % 2 !== 0){
+          console.log(i)
+      }
+  }
 }
+
+countToTwenty()
+countFromTwenty()
+countToN(13)
+countFromN(9)
+countEveryOdd(22)
